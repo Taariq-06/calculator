420f7a5707cf16178cd4492cc8c415f29d137b80
Taariq <taariqc2000@gmail.com>
2026-06-19 07:56:12 +0200

    finished calculationa and display logic

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const display = document.querySelector(".results");
-const screenNumber = display.innerHTML;
-const buttons = document.querySelectorAll(".calc-buttons button");
+const numbers = document.querySelectorAll(".calc-buttons button");
 
 let num1 = "";
 let num2 = "";
 let operation = "";
+let result = 0;
 
 const add = (a, b) => parseInt(a) + parseInt(b);
 const subtract = (a, b) => parseInt(a) - parseInt(b);
@@ -13,54 +13,90 @@ const divide = (a, b) => {
   if (b === "0") {
     return "Can't divide by 0";
   } else {
-    parseInt(a) / parseInt(b);
+    return parseInt(a) / parseInt(b);
   }
 };
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
+numbers.forEach((num) => {
+  num.addEventListener("click", (e) => {
     if (!isNaN(e.target.id)) {
       inputNumber(e.target.id);
-    }
-    if (num1.length !== 0 && operation === "") {
-        if (e.target.classList.contains("operator")) {
-            operation += e.target.innerHTML;
-    }
-    }
-    if( num1.length !== 0 && num2.length !== 0 && operation.length !== 0) {
-        if(e.target.id === "equal-to") {
-            operate(operation);
-        }
+    } else if (e.target.classList.contains("operator")) {
+      inputOperation(e.target.innerHTML);
+    } else if (e.target.id === "equal-to") {
+      if (num2.length !== 0 && operation.length !== 0) {
+        operate(operation);
+      }
     }
   });
 });
 
 const inputNumber = (num) => {
-  if (display.innerHTML < 12) {
+  const screenNumber = display.textContent;
+  if (screenNumber.length < 11) {
     if (operation === "") {
       num1 += num;
-      display.textContent = num1;
+      display.textContent = parseInt(num1).toString();
+      console.log("num1", num1);
     } else {
-        num2 += num;
+      num2 += num;
+      display.textContent =
+        parseInt(screenNumber).toString() +
+        operation +
+        parseInt(num2).toString();
+      console.log("num2", num2);
     }
-    display.innerHTML = num1
   }
 };
 
+const inputOperation = (op) => {
+  if (display.textContent.length < 11) {
+    if (num1.length !== 0) {
+      if (operation.length === 0) {
+        operation = op;
+        display.textContent += operation;
+        console.log("operator", operation);
+      }
+    }
+  }
+};
 
 const operate = (op) => {
   switch (op) {
     case "+":
-      console.log(add(num1, num2));
+      result = add(num1, num2);
+      display.textContent = result;
+      numbers.forEach((num) => {
+        num.addEventListener("click", (e) => {
+          if (e.target.classList.contains("operator")) {
+            num1 = result;
+            inputNumber(result);
+          }
+        });
+      });
+      num1 = "";
+      num2 = "";
+      operation = "";
       break;
     case "-":
-      console.log(subtract(num1, num2));
+      result = subtract(num1, num2);
+      display.textContent = result;
+      num1 = result;
+      num2 = "";
+      operation = "";
       break;
     case "*":
-      console.log(multiply(num1, num2));
+      result = multiply(num1, num2);
+      display.textContent = result;
+      num1 = result;
+      num2 = "";
+      operation = "";
       break;
     case "/":
-      console.log(divide(num1, num2));
+      result = divide(num1, num2);
+      display.textContent = result;
+      num1 = result;
+      num2 = "";
+      operation = "";
       break;
   }
 };
